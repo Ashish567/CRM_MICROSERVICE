93df6874284d50ac7c13e27f7114dba429e5def0
Ashish567 <djdaydreamer.singh4@gmail.com>
2023-09-18 01:00:54 +0530

    still learning nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,68 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
-
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import helmet from 'helmet';
+import cors from 'cors';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import * as Joi from 'joi';
+// import * as path from 'path';
+// const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [configuration],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+      validationOptions: {
+        allowUnknown: false,
+        abortEarly: true,
+      },
+    }),
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+    UserModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, cors(), helmet())
+      .forRoutes({ path: 'users', method: RequestMethod.ALL });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, PartialGraphHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import * as csurf from 'csurf';
+import fs from 'fs';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
+      snapshot: true,
+      abortOnError: false,
       transport: Transport.TCP,
       options: {
         port: 3000,
       },
     },
   );
+  app.useGlobalFilters(csurf());
   await app.listen();
 }
-bootstrap();
+bootstrap().catch((err: Error) => {
+  console.log(err);
+  fs.writeFileSync('graph.json', PartialGraphHost.toString() ?? '');
+  process.exit(1);
+});
